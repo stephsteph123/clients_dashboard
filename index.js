b49353f5369981ffb5a3ce76f1fac12508dc52e1
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-06-08 15:09:24 -0400

    clean up

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-let api = "https://my.api.mockaroo.com/users?key=9ea4a810";
+let api = "https://my.api.mockaroo.com/contacts.json?key=3203fe50";
 let card_body = document.getElementById("id_card_body");
 profiles = [];
+console.log(card_body)
 
-fetch(api).then(response => response.json()).then(data => {
+fetch(api)
+.then(response => response.json())
+.then(data => {
     profiles = data;
     getContacts(profiles);
 });
@@ -23,18 +26,4 @@ function deleteMe(event) {
     card_body.innerHTML = "";
     getContacts(profiles);
 }
-
-getContacts();
-
-
-// function getContacts() {
-//     fetch(api).then(response => response.json()).then(data => {
-//         data.forEach(function (contact) {
-//             card_body.innerHTML += `<div class="avatar">
-//             <img src="${contact.url}" alt="" />
-//             <span class="name">${contact.name}</span>
-//             </div>`;
-//         });
-//     });
-// }
 
