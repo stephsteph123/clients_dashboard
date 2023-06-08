899f1a97adbf4f6f77fe0cd342b670d86aeed436
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-06-08 17:45:35 -0400

    added API for peojects

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-let api = "https://my.api.mockaroo.com/contacts.json?key=3203fe50";
-let card_body = document.getElementById("id_card_body");
+// contacts
+let contactsApi = "https://my.api.mockaroo.com/contacts.json?key=3203fe50";
+let contact_card_body = document.getElementById("id_contact_card_body");
 profiles = [];
-console.log(card_body)
 
-fetch(api)
+fetch(contactsApi)
 .then(response => response.json())
 .then(data => {
     profiles = data;
@@ -12,7 +12,7 @@ fetch(api)
 
 function getContacts(contacts) {
     contacts.forEach(function (contact) {
-        card_body.innerHTML += `<div class="avatar">
+        contact_card_body.innerHTML += `<div class="avatar">
         <button onclick="deleteMe(event)">Remove</button>
         <img src="${contact.url}" alt="" />
         <span class="name">${contact.name}</span>
@@ -23,7 +23,28 @@ function getContacts(contacts) {
 function deleteMe(event) {
     let name = event.currentTarget.parentElement.children[2].innerHTML;
     profiles = profiles.filter((profile) => !(profile.name === name));
-    card_body.innerHTML = "";
+    contact_card_body.innerHTML = "";
     getContacts(profiles);
 }
 
+// projects
+let projectsApi = "https://my.api.mockaroo.com/projects.json?key=3203fe50";
+let project_card_body = document.getElementById("id_card_body");;
+projects = [];
+
+
+fetch(projectsApi)
+.then(response => response.json())
+.then(data => {
+    projects = data;
+    getProjects(projects);
+});
+
+function getProjects(projects){
+    projects.forEach(function (project) {
+        project_card_body.innerHTML += `<div class="stat-line">
+        <span class="stat-name">${project.project_name}</span>
+        <span class="stat-percent">${project.completed}%</span>
+        </div>`;
+    });
+}
