446c04d7979d53dde8380d108e7b7063f9656ff8
stephsteph123 <114449532+stephsteph123@users.noreply.github.com>
2023-07-21 14:56:35 -0400

    add API auth header to GET Tasks

--- clients_dashboard/src/hooks/useFetchTasks.js
+++ clients_dashboard/src/hooks/useFetchTasks.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+const axios = require("axios");
 
 function useFetchTasks() {
   const [tasks, setTasks] = useState([]);
@@ -6,21 +7,31 @@ function useFetchTasks() {
 
   useEffect(() => {
     const taskApi = "http://127.0.0.1:8090/api/collections/tasks/records/";
+    localStorage.setItem("token", "hello")
+    const token = localStorage.getItem("token")
 
-    fetch(taskApi)
+    fetch(taskApi, {
+      method: "GET",
+      headers: { "Content-Type": "application/STEPH", "authorization": `"Bearer ${token}"` }
+    })
       .then((response) => {
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
         return response.json();
       })
-      .then(data => {
+      .then((data) => {
         setTasks(data.items);
       })
-      .catch(error => {
+      .catch((error) => {
         setError(error.message);
       });
   }, []);
+
+  // this.axios.get("http://127.0.0.1:8090/api/collections/tasks/records/")
+  // .then(response => {
+  //   console.log(response)
+  // })
 
   return { tasks, error };
 }
